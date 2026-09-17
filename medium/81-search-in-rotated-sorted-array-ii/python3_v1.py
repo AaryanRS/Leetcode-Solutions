96c251e7fc49534f884e01af5f8ff4ef7e464701
Aaryan Shelar <aaryan.r.shelar5418@gmail.com>
2026-09-17 22:36:15 +0530

    Add solution: 81. Search in Rotated Sorted Array II (python3, v1)

---
+++ medium/81-search-in-rotated-sorted-array-ii/python3_v1.py
@@ -0,0 +1,29 @@
+# Pushed: 2026-09-17 17:04:57 UTC
+# Difficulty: Medium
+# Runtime: 0 ms
+# Memory: 19.5 MB
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> bool:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low+high)//2
+            if nums[mid] == target:
+                return True
+            if nums[low] == nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+                continue
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid-1
+                else:
+                    low = mid+1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid+1
+                else:
+                    high = mid-1
+        return False
